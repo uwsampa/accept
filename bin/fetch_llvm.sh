fe3f572ccb9c396d053f0755f8b893dec6836e91
Adrian Sampson <adrian@radbox.org>
2013-01-12 13:13:59 -0800

    move common checker stuff to checkerlib repo

--- bin/fetch_llvm.sh
+++ bin/fetch_llvm.sh
@@ -8,11 +8,3 @@ mv llvm-3.2.src llvm
 # Get (hacked) Clang.
 cd llvm/tools
 git clone git@bitbucket.org:sampa/clang-qual.git clang
- 
-# Compiler pass.
-cd ../lib/Transforms
-hg clone ssh://bicycle//projects/sampa/epa/enerc
-
-# Applications.
-cd ../../..
-hg clone ssh://bicycle//projects/sampa/epa/enerc-apps
