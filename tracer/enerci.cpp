9b92ef8297f92200a14d3050e2ef85752c984d71
Adrian Sampson <adrian@radbox.org>
2013-01-21 13:54:53 -0800

    remove a bunch of unnecessary cruft

--- tracer/enerci.cpp
+++ tracer/enerci.cpp
@@ -1,19 +1,10 @@
-#define DEBUG_TYPE "lli"
 #include "llvm/LLVMContext.h"
 #include "llvm/Module.h"
-#include "llvm/Type.h"
-#include "llvm/Bitcode/ReaderWriter.h"
-#include "llvm/ExecutionEngine/GenericValue.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/IRReader.h"
 #include "llvm/Support/PrettyStackTrace.h"
-#include "llvm/Support/raw_ostream.h"
-#include "llvm/Support/Format.h"
-#include "llvm/Support/Process.h"
 #include "llvm/Support/Signals.h"
-#include "llvm/Support/Debug.h"
 #include <cerrno>
-
 #include "ExecutionEngine/Interpreter/Interpreter.h"
 
 using namespace llvm;
@@ -26,9 +17,6 @@ namespace {
   InputArgv(cl::ConsumeAfter, cl::desc("<program arguments>..."));
 }
 
-//===----------------------------------------------------------------------===//
-// main Driver function
-//
 int main(int argc, char **argv, char * const *envp) {
   sys::PrintStackTraceOnErrorSignal();
   PrettyStackTraceProgram X(argc, argv);
@@ -38,7 +26,7 @@ int main(int argc, char **argv, char * const *envp) {
   cl::ParseCommandLineOptions(argc, argv,
                               "EnerC execution tracer\n");
 
-  // Load the bitcode...
+  // Load the bitcode.
   SMDiagnostic Err;
   Module *Mod = ParseIRFile(InputFile, Err, Context);
   if (!Mod) {
@@ -46,7 +34,7 @@ int main(int argc, char **argv, char * const *envp) {
     return 1;
   }
 
-  // If not jitting lazily, load the whole bitcode file eagerly too.
+  // Load the whole bitcode file eagerly.
   std::string ErrorMsg;
   if (Mod->MaterializeAllPermanently(&ErrorMsg)) {
     errs() << argv[0] << ": bitcode didn't read correctly.\n";
@@ -54,48 +42,28 @@ int main(int argc, char **argv, char * const *envp) {
     exit(1);
   }
 
-  ExecutionEngine *EE = 0;
-  EE = Interpreter::create(Mod, &ErrorMsg);
+  // Create the interpreter.
+  ExecutionEngine *EE = Interpreter::create(Mod, &ErrorMsg);
   if (!ErrorMsg.empty()) {
     errs() << argv[0] << ": error creating EE: " << ErrorMsg << "\n";
     exit(1);
   }
 
-  // Otherwise, if there is a .bc suffix on the executable strip it off, it
-  // might confuse the program.
+  // Remove ".bc" suffix from input bitcode name and use it as argv[0].
   if (StringRef(InputFile).endswith(".bc"))
     InputFile.erase(InputFile.length() - 3);
-
-  // Add the module's name to the start of the vector of arguments to main().
   InputArgv.insert(InputArgv.begin(), InputFile);
 
-  // Call the main function from M as if its signature were:
-  //   int main (int argc, char **argv, const char **envp)
-  // using the contents of Args to determine argc & argv, and the contents of
-  // EnvVars to determine envp.
-  //
+  // Get the main function from the module.
   Function *EntryFn = Mod->getFunction("main");
   if (!EntryFn) {
     errs() << '\'' << "main" << "\' function not found in module.\n";
     return -1;
   }
 
-  // If the program doesn't explicitly call exit, we will need the Exit
-  // function later on to make an explicit call, so get the function now.
-  Constant *Exit = Mod->getOrInsertFunction("exit", Type::getVoidTy(Context),
-                                                    Type::getInt32Ty(Context),
-                                                    NULL);
-
   // Reset errno to zero on entry to main.
   errno = 0;
 
-  int Result;
-  // Trigger compilation separately so code regions that need to be 
-  // invalidated will be known.
-  (void)EE->getPointerToFunction(EntryFn);
-
   // Run main.
-  Result = EE->runFunctionAsMain(EntryFn, InputArgv, envp);
-
-  return Result;
+  return EE->runFunctionAsMain(EntryFn, InputArgv, envp);
 }
