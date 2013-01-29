f6c5ea9cd8f9663dc5bed1615866ab41e2754e0c
Adrian Sampson <adrian@radbox.org>
2013-01-29 15:44:37 -0800

    disable instrumentation and runtime library

--- bin/profile.sh
+++ bin/profile.sh
@@ -3,8 +3,11 @@ here=`dirname $0`
 
 enercdir=$here/..
 bin=$enercdir/build/built/bin
-proflib=$enercdir/build/enerc/rt/enercrt.bc
+
+# Runtime library disabled for now since we're using an interpretation (instead
+# of instrumentation) approach.
+# proflib=$enercdir/build/enerc/rt/enercrt.bc
 
 bcfile=$1
 shift
-$bin/llvm-link $proflib $bcfile | $bin/opt -strip | $bin/lli - $@
+$bin/llvm-link $bcfile | $bin/opt -strip | $bin/enerci - $@
