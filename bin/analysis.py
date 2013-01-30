b6a2f0e6dcfd98db7691bd9cc39c6da8d8408736
Adrian Sampson <adrian@radbox.org>
2013-01-29 17:07:34 -0800

    human-readable stats output

--- bin/analysis.py
+++ bin/analysis.py
@@ -1,5 +1,5 @@
 from __future__ import division
-import json
+from __future__ import print_function
 import sys
 from collections import namedtuple, Counter
 
@@ -7,6 +7,10 @@ InstructionInfo = namedtuple('InstructionInfo', ['approx', 'elidable'])
 
 def analyze(info_fn="enerc_info.txt",
             trace_fn="enerc_trace.txt"):
+    counts = Counter(['dynamic approx', 'dynamic elidable', 'dynamic total',
+                      'static approx', 'static elidable', 'static total'])
+
+    # Static information and statistics.
     inst_info = {}
     with open(info_fn) as f:
         for line in f:
@@ -14,29 +18,36 @@ def analyze(info_fn="enerc_info.txt",
             if line:
                 num, approx, elidable = line.split()
                 approx, elidable = (bool(int(v)) for v in (approx, elidable))
+                counts['static approx'] += approx
+                counts['static elidable'] += elidable
+                counts['static total'] += 1
                 inst_info[int(num)] = InstructionInfo(approx, elidable)
     
-    counts = Counter(['approx', 'elidable', 'total'])
+    # Dynamic statistics.
     with open(trace_fn) as f:
         for line in f:
             line = line.strip()
             if line:
                 num = int(line)
-
-                if inst_info[num].elidable:
-                    counts["elidable"] += 1
-                if inst_info[num].approx:
-                    counts["approx"] += 1
-                counts["total"] += 1
-    
-    counts["proportion approx"] = counts["approx"] / counts["total"]
-    counts["proportion elidable"] = counts["elidable"] / counts["total"]
+                counts['dynamic approx'] += inst_info[num].approx
+                counts['dynamic elidable'] += inst_info[num].elidable
+                counts['dynamic total'] += 1
     
     return counts
 
-def _dump(obj):
-    json.dump(obj, sys.stdout, indent=4, sort_keys=True)
+def summarize(counts):
+    print('Static instructions: {}'.format(counts['static total']))
+    print('{:.2%} approx; {:.2%} elidable'.format(
+        counts['static approx'] / counts['static total'],
+        counts['static elidable'] / counts['static total'],
+    ))
+    print()
+    print('Dynamic instructions: {}'.format(counts['dynamic total']))
+    print('{:.2%} approx; {:.2%} elidable'.format(
+        counts['dynamic approx'] / counts['dynamic total'],
+        counts['dynamic elidable'] / counts['dynamic total'],
+    ))
 
 if __name__ == "__main__":
     args = sys.argv[1:]
-    _dump(analyze(*args))
+    summarize(analyze())
