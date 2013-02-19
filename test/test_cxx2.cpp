0297a66e8639a73dbfe8d2a1d6b1213e3c1d86a0
Adrian Sampson <adrian@radbox.org>
2013-02-19 12:58:02 -0800

    correctly type method call params

--- test/test_cxx2.cpp
+++ test/test_cxx2.cpp
@@ -3,10 +3,18 @@
 struct mystruct {
     APPROX int field;
 };
+class myclass {
+public:
+  int meth(APPROX int *x);
+};
+
 int main() {
     APPROX int x = 2;
     struct mystruct s;
     s.field = 5;
     struct mystruct s2 = s;
     struct mystruct s3 = {5};
+
+    myclass obj;
+    obj.meth(&x);
 }
