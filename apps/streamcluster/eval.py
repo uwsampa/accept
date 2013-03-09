40198b05504460184a8ace1fce53834988424915
Adrian Sampson <adrian@radbox.org>
2013-03-08 22:26:38 -0800

    output memoization

--- apps/streamcluster/eval.py
+++ apps/streamcluster/eval.py
@@ -1,5 +1,34 @@
 import subprocess
 import time
+import dumptruck
+import functools
+
+dumptruck.PYTHON_SQLITE_TYPE_MAP[tuple] = u'json text'
+
+class Memoized(object):
+    def __init__(self, dbname, func):
+        self.func = func
+        self.dt = dumptruck.DumpTruck(dbname)
+        self.table = 'memo_{}'.format(func.__name__)
+
+    def __call__(self, *args, **kwargs):
+        if self.table in self.dt.tables():
+            memoized = self.dt.execute(
+                'SELECT res FROM {} WHERE args=? AND kwargs=? '
+                'LIMIT 1'.format(self.table),
+                (args, kwargs)
+            )
+            if memoized:
+                return memoized[0]['res']
+
+        res = self.func(*args, **kwargs)
+        self.dt.insert(
+            {'args': args, 'kwargs': kwargs, 'res': res},
+            self.table
+        )
+        return res
+
+memoize = functools.partial(Memoized, 'memo.db')
 
 def load(fn):
     centers = []
@@ -40,11 +69,14 @@ def build(approx=False):
         build_cmd.append('CLANGARGS=-mllvm -accept-relax')
     subprocess.check_call(build_cmd)
 
+@memoize
+def build_and_execute(approx=False):
+    build(approx)
+    return execute()
+
 def main():
-    build(False)
-    pout, ptime = execute()
-    build(True)
-    aout, atime = execute()
+    pout, ptime = build_and_execute(False)
+    aout, atime = build_and_execute(True)
     score(pout, aout)
     print('{:.2f} vs. {:.2f}: {:.2f}x'.format(ptime, atime, ptime / atime))
 
