c5669ef229723ff3f0537ab6d0490528d1666c06
Adrian Sampson <adrian@radbox.org>
2013-04-09 15:08:39 -0700

    repo cleanup

---
+++ test/test_implicit.c
@@ -0,0 +1,26 @@
+// RUN: clang -fsyntax-only -Xclang -verify %s
+// expected-no-diagnostics
+
+#include <enerc.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    // malloc
+    APPROX int *xp;
+    int *yp;
+    yp = (int*)malloc(2);
+    xp = (int*)malloc(2);
+
+    // free
+    free(yp);
+    free(xp);
+
+    // memcpy
+    memcpy(yp, yp, 1);
+    memcpy(xp, xp, 1);
+
+    // memset
+    memset(xp, 0, 1);
+    memset(yp, 0, 1);
+}
