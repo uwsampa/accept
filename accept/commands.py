f693816bedd90ddeb37dc06a7266fa5e42b4c058
Adrian Sampson <adrian@radbox.org>
2013-04-10 11:14:43 -0700

    "accept log" command for viewing compilation log

--- accept/commands.py
+++ accept/commands.py
@@ -5,12 +5,16 @@ import argh
 import logging
 import sys
 from . import experiments
+from . import core
+from . import cwmemo
 
 
 LOCAL_REPS = 1
 CLUSTER_REPS = 5
 APPS = ['streamcluster', 'blackscholes', 'sobel']
 
+
+# Run the experiments.
 
 @argh.arg('appnames', metavar='NAME', default=APPS, nargs='*', type=unicode,
           help='applications')
@@ -20,14 +24,33 @@ def exp(appnames, verbose=False, cluster=False, force=False, reps=None):
     if not reps:
         reps = CLUSTER_REPS if cluster else LOCAL_REPS
 
-    logging.getLogger().addHandler(logging.StreamHandler(sys.stderr))
-
     for appname in appnames:
         print(appname)
         experiments.evaluate(appname, verbose, cluster, force, reps)
 
 
+# Get the compilation log.
+
+def get_log(directory):
+    """Build the benchmark in `directory` and return the contents of the
+    compilation log.
+    """
+    with core.chdir(directory):
+        with core.sandbox(True):
+            core.build()
+            with open('accept_log.txt') as f:
+                return f.read()
+
+@argh.arg('appdir', nargs='?', help='application directory')
+def log(appdir='.'):
+    with cwmemo.get_client(cluster=False, force=False) as client:
+        client.submit(get_log, appdir)
+        logtxt = client.get(get_log, appdir)
+    print(logtxt)
+
+
 if __name__ == '__main__':
+    logging.getLogger().addHandler(logging.StreamHandler(sys.stderr))
     parser = argh.ArghParser()
-    parser.add_commands([exp])
+    parser.add_commands([exp, log])
     parser.dispatch()
