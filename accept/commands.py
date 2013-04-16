6698b1bb8fcbec40005d45f9ea71d1407b589165
Adrian Sampson <adrian@radbox.org>
2013-04-16 15:27:48 -0700

    command to show approximate outputs

--- accept/commands.py
+++ accept/commands.py
@@ -102,11 +102,38 @@ def precise(appdir='.'):
     for desc in descs.values():
         print('  ', desc)
 
+@argh.arg('num', nargs='?', type=int, help='which configuration')
+@argh.arg('appdir', nargs='?', help='application directory')
+def approx(num=None, appdir='.'):
+    appdir = exppath(appdir)
+    with _client:
+        _client.submit(core.build_and_execute, appdir, None, 0)
+        _, ptime, _, base_config, descs = \
+                _client.get(core.build_and_execute, appdir, None, 0)
+
+        # Get configurations (and possibly narrow to only one).
+        configs = list(core.permute_config(base_config))
+        if num is not None:
+            configs = [configs[num]]
+        configs = [configs[num]] if num is not None else configs
+
+        for config in configs:
+            _client.submit(core.build_and_execute,
+                appdir, config, 0,
+                timeout=ptime * experiments.TIMEOUT_FACTOR
+            )
+            aout, atime, _, _, _ = \
+                _client.get(core.build_and_execute, appdir, config, 0)
+
+            print('output:', aout)
+            print('time: {:.2f}'.format(atime))
+            print()
+
 
 def main():
     logging.getLogger().addHandler(logging.StreamHandler(sys.stderr))
     parser = argh.ArghParser()
-    parser.add_commands([exp, log, build, precise])
+    parser.add_commands([exp, log, build, precise, approx])
     parser.add_argument('--cluster', '-c', default=False, action='store_true',
                         help='execute on Slurm cluster')
     parser.add_argument('--force', '-f', default=False, action='store_true',
