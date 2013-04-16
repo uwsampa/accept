ed7bda704d507cc909b91020976088b99bc8eedd
Adrian Sampson <adrian@radbox.org>
2013-04-15 21:50:50 -0700

    complete annotation for canneal

--- apps/canneal/MersenneTwister.h
+++ apps/canneal/MersenneTwister.h
@@ -271,7 +271,7 @@ inline void MTRand::seed()
 		register int i = N;
 		register bool success = true;
 		while( success && i-- )
-			success = fread( s++, sizeof(uint32), 1, urandom );
+			success = fread( ENDORSE(s++), sizeof(uint32), 1, urandom );
 		fclose(urandom);
 		if( success ) { seed( bigSeed, N );  return; }
 	}
