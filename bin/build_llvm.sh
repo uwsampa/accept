25bfc01c6167ce41cacdf34c97298f7983155917
Andre Baixo <andreolb@cs.washington.edu>
2013-04-24 14:34:58 -0700

    Replace original AliasAnalysis files by the new ones during the build process

--- bin/build_llvm.sh
+++ bin/build_llvm.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 here=$(cd "$(dirname "$0")"; pwd)
 base=$here/..
+modified_llvm_files=$base/modified_LLVM_files
 
 # This is where all build products will be installed.
 destdir=$base/build/built
@@ -9,6 +10,8 @@ mkdir -p $destdir
 # Build LLVM "out-of-source" in a scratch directory.
 builddir=$base/build/llvm
 mkdir -p $builddir
+cp $modified_llvm_files/BasicAliasAnalysis.cpp $base/llvm/lib/Analysis
+cp $modified_llvm_files/TypeBasedAliasAnalysis.cpp $base/llvm/lib/Analysis
 cd $builddir
 cmake -G Ninja -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_INSTALL_PREFIX:PATH=$destdir ../../llvm
 ninja install
