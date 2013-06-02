a112c0f89e166a1c483df5403134bb814db69032
Adrian Sampson <adrian@radbox.org>
2013-06-02 12:07:24 -0700

    always use tiny tags

--- include/enerc.h
+++ include/enerc.h
@@ -1,17 +1,8 @@
 #ifndef ENERC_H
 #define ENERC_H
 
-#ifdef __MSP430__
-#define ENERC_TINY_TAGS
-#endif
-
-#ifdef ENERC_TINY_TAGS
 #define TAG_ENDORSEMENT 39945
 #define TAG_DEDORSEMENT 39946
-#else
-#define TAG_ENDORSEMENT 9946037276205
-#define TAG_DEDORSEMENT 9946037276206
-#endif
 
 #define APPROX __attribute__((qual("approx")))
 #define ENDORSE(e) (_Pragma("clang diagnostic push") \
