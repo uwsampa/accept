36c9a351123f4bb73acb13acc403f0ca55a4cc7f
Ben Ransford <ransford@cs.washington.edu>
2013-07-22 17:00:11 -0700

    drastically simplify msp430-accel benchmark:  - 1-dimensional, not 3-dimensional, accelerometer  - fewer accelerometer readings total  - remove superfluous stuff that is not meaningful in simulation

--- apps/msp430-accel/accel.c
+++ apps/msp430-accel/accel.c
@@ -5,9 +5,9 @@
 #include <msp430.h>
 #include "dlwisp41.h"
 
-#define NUM_READINGS 1000
+#define NUM_READINGS 250u
 
-unsigned numreadings = 0;
+APPROX unsigned int numreadings = 0;
 
 // status register &= ~i
 inline void __bic_status_register (unsigned i) {
@@ -19,25 +19,14 @@ inline void __bis_status_register (unsigned i) {
     asm volatile ("BIS.W %0, R2" ::"m"(i));
 }
 
-typedef struct {
-    APPROX unsigned x;
-    APPROX unsigned y;
-    APPROX unsigned z;
-} accel_reading;
-
-void read_sensor (accel_reading *reading) {
-    unsigned char i;
-
-    // turn off comparator
-    P1OUT &= ~RX_EN_PIN;
+APPROX unsigned read_accel() {
+    APPROX unsigned char i;
+    APPROX unsigned reading;
 
     // slow down clock
     BCSCTL1 = XT2OFF + RSEL1; // select internal resistor (still has effect when
     // DCOR=1)
     DCOCTL = DCO1+DCO0; // set DCO step.
-
-    if(!is_power_good())
-        sleep();
 
     // Clear out any lingering voltage on the accelerometer outputs
     ADC10AE0 = 0;
@@ -52,35 +41,16 @@ void read_sensor (accel_reading *reading) {
 
     // a little time for regulator to stabilize active mode current AND
     // filter caps to settle.
-    for(i = 0; i < 225; i++);
+    for(i = 0; ENDORSE(i < 225); i++);
     RECEIVE_CLOCK;
 
-    // x
     ADC10CTL0 &= ~ENC;
     ADC10CTL0 = SREF_0 + ADC10SHT_1 + ADC10ON;
     ADC10CTL1 = ADC10DIV_2 + ADC10SSEL_0 + SHS_0 + CONSEQ_0 + INCH_ACCEL_X;
     ADC10CTL0 |= ENC;
     ADC10CTL0 |= ADC10SC;
     while (ADC10CTL1 & ADC10BUSY);    // wait while ADC finishes work
-    reading->x = ADC10MEM & 0x3ff;
-
-    // y
-    ADC10CTL0 &= ~ENC;
-    ADC10CTL0 = SREF_0 + ADC10SHT_1 + ADC10ON;
-    ADC10CTL1 = ADC10DIV_2 + ADC10SSEL_0 + SHS_0 + CONSEQ_0 + INCH_ACCEL_Y;
-    ADC10CTL0 |= ENC;
-    ADC10CTL0 |= ADC10SC;
-    while (ADC10CTL1 & ADC10BUSY);    // wait while ADC finishes work
-    reading->y = ADC10MEM & 0x3ff;
-
-    // z
-    ADC10CTL0 &= ~ENC;
-    ADC10CTL0 = SREF_0 + ADC10SHT_1 + ADC10ON;
-    ADC10CTL1 = ADC10DIV_2 + ADC10SSEL_0 + SHS_0 + CONSEQ_0 + INCH_ACCEL_Z;
-    ADC10CTL0 |= ENC;
-    ADC10CTL0 |= ADC10SC;
-    while (ADC10CTL1 & ADC10BUSY);    // wait while ADC finishes work
-    reading->z = ADC10MEM & 0x3ff;
+    reading = ADC10MEM & 0x3ff;
 
     // Power off sensor and adc
     P1DIR &= ~ACCEL_POWER;
@@ -89,34 +59,7 @@ void read_sensor (accel_reading *reading) {
     ADC10CTL1 = 0;       // turn adc off
     ADC10CTL0 = 0;       // turn adc off
 
-    // Store sensor read count
-    numreadings++;
-}
-
-unsigned short is_power_good (void) {
-    return P2IN & VOLTAGE_SV_PIN;
-}
-
-inline void sleep (void) {
-    P1OUT &= ~RX_EN_PIN;
-    // enable port interrupt for voltage supervisor
-    P2IES = 0;
-    P2IFG = 0;
-    P2IE |= VOLTAGE_SV_PIN;
-    P1IE = 0;
-    P1IFG = 0;
-    TACTL = 0;
-
-    __bic_status_register(GIE); // temporarily disable GIE so we can sleep and enable interrupts
-    // at the same time
-    P2IE |= VOLTAGE_SV_PIN; // Enable Port 2 interrupt
-
-    if (is_power_good())
-        P2IFG = VOLTAGE_SV_PIN;
-
-    __bis_status_register(LPM4_bits | GIE);
-
-    return;
+    return reading;
 }
 
 #ifdef __clang__
@@ -126,22 +69,18 @@ inline void sleep (void) {
 __attribute__((section(".init9"), aligned(2)))
 #endif
 int main (void) {
-    accel_reading cur_reading;
-    APPROX unsigned max_x, max_y, max_z;
-    APPROX unsigned min_x, min_y, min_z;
-    APPROX unsigned avg_x, avg_y, avg_z;
-    unsigned i;
+    WDTCTL = WDTPW + WDTHOLD; // stop watchdog timer
 
-    for (i = 0; i < NUM_READINGS; ++i) {
-        read_sensor(&cur_reading); // ACCEPT_PERMIT
+    APPROX unsigned cur_reading = 0;
+    APPROX unsigned max = 0;
+    APPROX unsigned min = 0xffffu;
+    APPROX unsigned avg = 0xbeefu;
 
-        if (ENDORSE(cur_reading.x > max_x)) max_x = cur_reading.x;
-        if (ENDORSE(cur_reading.y > max_y)) max_y = cur_reading.y;
-        if (ENDORSE(cur_reading.z > max_z)) max_z = cur_reading.z;
+    for (; ENDORSE(numreadings != NUM_READINGS); ++numreadings) {
+        cur_reading = read_accel(); // ACCEPT_PERMIT
 
-        if (ENDORSE(cur_reading.x < min_x)) min_x = cur_reading.x;
-        if (ENDORSE(cur_reading.y < min_y)) min_y = cur_reading.y;
-        if (ENDORSE(cur_reading.z < min_z)) min_z = cur_reading.z;
+        if (ENDORSE(cur_reading > max)) max = cur_reading;
+        if (ENDORSE(cur_reading < min)) min = cur_reading;
 
         // TODO: moving average
     }
