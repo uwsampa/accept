d99d120c971eb73daa8f751564321204a584d84f
Adrian Sampson <adrian@radbox.org>
2014-02-22 17:12:07 -0800

    ACCEPT build script: check that LLVM is built

--- bin/build_enerc.sh
+++ bin/build_enerc.sh
@@ -5,6 +5,12 @@ base=$here/..
 destdir=$base/build/built
 mkdir -p $destdir
 
+# Check to make sure LLVM is built where we're expecting it.
+if [ ! -e $destdir/share/llvm/cmake/LLVMConfig.cmake ] ; then
+    echo "LLVM not present in build/built/. Please run build_llvm.sh first."
+    exit 1
+fi
+
 builddir=$base/build/enerc
 mkdir -p $builddir
 cd $builddir
