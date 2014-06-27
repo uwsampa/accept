674df4d21464ec223661b5e3bbf7e4538c9f9d90
Adrian Sampson <adrian@radbox.org>
2014-06-27 14:18:47 -0700

    Use FPGA cycle counts for ROI timing (#17)
    This is the whole point of the separate runtime implementation for this
    platform.

--- rt/acceptrt.zynq.c
+++ rt/acceptrt.zynq.c
@@ -1,6 +1,18 @@
 // Zynq platform: use FPGA cycle timer and print to stdout.
+#include <stdio.h>
+
+// Memory mapped hardware counter.
+#define gpio_addr       0xe000a048
+#define rd_fpga_clk()   *((volatile unsigned int*)gpio_addr)
+
+unsigned int accept_clock_begin;
+
 void accept_roi_begin() {
+    accept_clock_begin = rd_fpga_clk();
 }
 
 void accept_roi_end() {
+    unsigned int clock_end = rd_fpga_clk();
+    unsigned int elapsed = accept_clock_begin - clock_end;
+    printf("ACCEPT TIME: %u\n", elapsed);
 }
