aef99ae03e76f179a5dc4252e7a73f0e6af5fbb9
Adrian Sampson <adrian@radbox.org>
2014-07-09 18:41:17 -0700

    Temporarily back out modifications to bbfdetect.c
    Reverts commit 2b6a8fece0b8dcab4d7735dacd096dc63ce47f51 to this file to return
    it to its pristine state.

--- apps/ccv/bin/bbfdetect.c
+++ apps/ccv/bin/bbfdetect.c
@@ -2,12 +2,6 @@
 #include <sys/time.h>
 #include <ctype.h>
 
-// added block
-#include <enerc.h>
-#include <stdio.h>
-#include <stdlib.h>
-// added block
-
 unsigned int get_current_time()
 {
 	struct timeval tv;
@@ -17,26 +11,16 @@ unsigned int get_current_time()
 
 int main(int argc, char** argv)
 {
-        // added block
-        FILE* file;
-        char *output_file = "my_output.txt";
-        int i, ret_val;
-        ccv_dense_matrix_t* image = 0;
-        ccv_array_t* seq;
-        // added block
-
-        // added line
-        accept_roi_begin();
 	assert(argc >= 3);
-	// int i; removed line
+	int i;
 	ccv_enable_default_cache();
-	// ccv_dense_matrix_t* image = 0; removed line
+	ccv_dense_matrix_t* image = 0;
 	ccv_bbf_classifier_cascade_t* cascade = ccv_bbf_read_classifier_cascade(argv[2]);
 	ccv_read(argv[1], &image, CCV_IO_GRAY | CCV_IO_ANY_FILE);
 	if (image != 0)
 	{
 		unsigned int elapsed_time = get_current_time();
-		seq = ccv_bbf_detect_objects(image, &cascade, 1, ccv_bbf_default_params); // seq already declared above
+		ccv_array_t* seq = ccv_bbf_detect_objects(image, &cascade, 1, ccv_bbf_default_params);
 		elapsed_time = get_current_time() - elapsed_time;
 		for (i = 0; i < seq->rnum; i++)
 		{
@@ -44,43 +28,8 @@ int main(int argc, char** argv)
 			printf("%d %d %d %d %f\n", comp->rect.x, comp->rect.y, comp->rect.width, comp->rect.height, comp->classification.confidence);
 		}
 		printf("total : %d in time %dms\n", seq->rnum, elapsed_time);
-		// ccv_array_free(seq); removed line
-		// ccv_matrix_free(image); removed line
-
-                // added block
-                ccv_bbf_classifier_cascade_free(cascade);
-                ccv_disable_cache();
-                accept_roi_end();
-                file = fopen(output_file, "w");
-                if (file == NULL) {
-                  perror("fopen for write failed");
-                  return EXIT_FAILURE;
-                }
-                for (i = 0; i < seq->rnum; i++) {
-                  ccv_comp_t* comp = (ccv_comp_t*) ccv_array_get(seq, i);
-                  ret_val = fprintf(file, "%d\n%d\n%d\n%d\n%f\n", comp->rect.x, comp->rect.y, comp->rect.width,
-                      comp->rect.height, comp->classification.confidence);
-                  if (ret_val < 0) {
-                    perror("fprintf of coordinates and confidence failed");
-                    fclose(file);
-                    return EXIT_FAILURE;
-                  }
-                }
-                ret_val = fprintf(file, "%d\n%d\n", seq->rnum, elapsed_time);
-                if (ret_val < 0) {
-                  perror("fprintf of number and time failed");
-                  fclose(file);
-                  return EXIT_FAILURE;
-                }
-                ret_val = fclose(file);
-                if (ret_val != 0) {
-                  perror("fclose failed");
-                  return EXIT_FAILURE;
-                }
-                ccv_array_free(seq);
-                ccv_matrix_free(image);
-                // added block
-
+		ccv_array_free(seq);
+		ccv_matrix_free(image);
 	} else {
 		FILE* r = fopen(argv[1], "rt");
 		if (argc == 4)
@@ -98,48 +47,20 @@ int main(int argc, char** argv)
 				image = 0;
 				ccv_read(file, &image, CCV_IO_GRAY | CCV_IO_ANY_FILE);
 				assert(image != 0);
-				seq = ccv_bbf_detect_objects(image, &cascade, 1, ccv_bbf_default_params); // seq already declared above
+				ccv_array_t* seq = ccv_bbf_detect_objects(image, &cascade, 1, ccv_bbf_default_params);
 				for (i = 0; i < seq->rnum; i++)
 				{
 					ccv_comp_t* comp = (ccv_comp_t*)ccv_array_get(seq, i);
 					printf("%s %d %d %d %d %f\n", file, comp->rect.x, comp->rect.y, comp->rect.width, comp->rect.height, comp->classification.confidence);
 				}
-				// ccv_array_free(seq); removed line
-				// ccv_matrix_free(image); removed line
+				ccv_array_free(seq);
+				ccv_matrix_free(image);
 			}
 			free(file);
 			fclose(r);
 		}
-
-                // added block
-                ccv_bbf_classifier_cascade_free(cascade);
-                ccv_disable_cache();
-                accept_roi_end();
-                file = fopen(output_file, "w");
-                if (file == NULL) {
-                  perror("fopen for write failed");
-                  return EXIT_FAILURE;
-                }
-                for (i = 0; i < seq->rnum; i++) {
-                  ccv_comp_t* comp = (ccv_comp_t*) ccv_array_get(seq, i);
-                  ret_val = fprintf(file, "%d\n%d\n%d\n%d\n%f\n", comp->rect.x, comp->rect.y, comp->rect.width,
-                      comp->rect.height, comp->classification.confidence);
-                  if (ret_val < 0) {
-                    perror("fprintf of coordinates and confidence failed");
-                    fclose(file);
-                    return EXIT_FAILURE;
-                  }
-                }
-                ret_val = fclose(file);
-                if (ret_val != 0) {
-                  perror("fclose failed");
-                  return EXIT_FAILURE;
-                }
-                ccv_array_free(seq);
-                ccv_matrix_free(image);
-                // added block
 	}
-	// ccv_bbf_classifier_cascade_free(cascade); removed line
-	// ccv_disable_cache(); removed line
+	ccv_bbf_classifier_cascade_free(cascade);
+	ccv_disable_cache();
 	return 0;
 }
