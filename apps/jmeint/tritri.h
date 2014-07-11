aaefae1f64640bc62d9229a7a5d457ca1d166cc4
Adrian Sampson <adrian@radbox.org>
2014-07-11 12:34:51 -0700

    Add jmeint code from npu branch

---
+++ apps/jmeint/tritri.h
@@ -0,0 +1,16 @@
+/*
+ * tritri.h
+ *
+ *  Created on: Apr 25, 2012
+ *      Author: Hadi Esmaeilzadeh <hadianeh@cs.washington.edu>
+ */
+
+#ifndef TRITRI_H_
+#define TRITRI_H_
+
+#include <enerc.h>
+
+APPROX float tri_tri_intersect(float V0[3],float V1[3],float V2[3],
+                      float U0[3],float U1[3],float U2[3]);
+
+#endif /* TRITRI_H_ */
