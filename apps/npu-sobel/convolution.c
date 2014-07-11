ff3d9c87cfe4ea5480bb13a6020e9fc00ffb2f39
andreolb <andreolb@localhost.localdomain>
2014-07-11 14:58:27 -0700

    Add sobel (from the npu benchmarks)

---
+++ apps/npu-sobel/convolution.c
@@ -0,0 +1,43 @@
+#include "convolution.h"
+#include <math.h>
+#include <stdio.h>
+#include <enerc.h>
+
+static float kx[][3] =
+    { { -1, -2, -1},
+      {  0, 0,  0},
+      {  1, 2,  1}};
+
+static float ky[][3] =
+    { { -1, 0,  1},
+      { -2, 0,  2},
+      { -1, 0,  1}};
+
+float convolve(float w[][3], float k[][3]) {
+    int i;
+    int j;
+    float r;
+
+    r = 0;
+    for (j = 0; j < 3; j++)
+        for (i = 0; i < 3; i++)
+            r += w[i][j] * k[j][i];
+
+    return r;
+}
+
+APPROX float sobel(float w[][3]) {
+    float sx;
+    float sy;
+    float s;
+
+    sx = convolve(w, ky);
+    sy = convolve(w, kx);
+
+    s = sqrt(sx * sx + sy * sy);
+    if (s >= (256 / sqrt(256 * 256 + 256 * 256)))
+        s = 255 / sqrt(256 * 256 + 256 * 256);
+
+    return s ;
+}
+
