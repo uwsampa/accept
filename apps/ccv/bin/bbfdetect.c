47f6420aec90f56ac9fe1675b78e9f6302ea98b1
Joshua Yip <joshuay@uw.edu>
2014-07-24 13:45:27 -0700

    Modified bbfdetect.c and eval.py to compare coordinates

--- apps/ccv/bin/bbfdetect.c
+++ apps/ccv/bin/bbfdetect.c
@@ -1,12 +1,9 @@
 #include "ccv.h"
 #include <sys/time.h>
 #include <ctype.h>
-
-// added block
 #include <enerc.h>
 #include <stdio.h>
 #include <stdlib.h>
-// added block
 
 unsigned int get_current_time()
 {
@@ -17,26 +14,21 @@ unsigned int get_current_time()
 
 int main(int argc, char** argv)
 {
-        // added block
         FILE* file;
         char *output_file = "my_output.txt";
         int i, ret_val;
         ccv_dense_matrix_t* image = 0;
         ccv_array_t* seq;
-        // added block
 
-        // added line
         accept_roi_begin();
 	assert(argc >= 3);
-	// int i; removed line
 	ccv_enable_default_cache();
-	// ccv_dense_matrix_t* image = 0; removed line
 	ccv_bbf_classifier_cascade_t* cascade = ccv_bbf_read_classifier_cascade(argv[2]);
 	ccv_read(argv[1], &image, CCV_IO_GRAY | CCV_IO_ANY_FILE);
 	if (image != 0)
 	{
 		unsigned int elapsed_time = get_current_time();
-		seq = ccv_bbf_detect_objects(image, &cascade, 1, ccv_bbf_default_params); // seq already declared above
+		seq = ccv_bbf_detect_objects(image, &cascade, 1, ccv_bbf_default_params);
 		elapsed_time = get_current_time() - elapsed_time;
 		for (i = 0; i < seq->rnum; i++)
 		{
@@ -44,33 +36,39 @@ int main(int argc, char** argv)
 			printf("%d %d %d %d %f\n", comp->rect.x, comp->rect.y, comp->rect.width, comp->rect.height, comp->classification.confidence);
 		}
 		printf("total : %d in time %dms\n", seq->rnum, elapsed_time);
-		// ccv_array_free(seq); removed line
-		// ccv_matrix_free(image); removed line
-
-                // added block
                 ccv_bbf_classifier_cascade_free(cascade);
                 ccv_disable_cache();
                 accept_roi_end();
+                
                 file = fopen(output_file, "w");
                 if (file == NULL) {
                   perror("fopen for write failed");
                   return EXIT_FAILURE;
                 }
+                // latest changes
+                struct coordinates {
+                    APPROX int x;
+                    APPROX int y;
+                };
                 for (i = 0; i < seq->rnum; i++) {
                   ccv_comp_t* comp = (ccv_comp_t*) ccv_array_get(seq, i);
-                  ret_val = fprintf(file, "%d\n%d\n%d\n%d\n%f\n", comp->rect.x, comp->rect.y, comp->rect.width,
-                      comp->rect.height, comp->classification.confidence);
+                  struct coordinates upperleft, upperright, lowerleft, lowerright;
+                  upperleft.x = comp->rect.x;
+                  upperleft.y = comp->rect.y;
+                  upperright.x = comp->rect.x + comp->rect.width;
+                  upperright.y = upperleft.y;
+                  lowerleft.x = upperleft.x;
+                  lowerleft.y = upperleft.y + comp->rect.height;
+                  lowerright.x = upperright.x;
+                  lowerright.y = lowerleft.y;
+                  ret_val = fprintf(file, "%d %d\n%d %d\n%d %d\n%d %d\n", upperleft.x, upperleft.y, upperright.x, upperright.y,
+                      lowerright.x, lowerright.y, lowerleft.x, lowerleft.y);
+                  // latest changes
                   if (ret_val < 0) {
-                    perror("fprintf of coordinates and confidence failed");
+                    perror("fprintf of coordinates failed");
                     fclose(file);
                     return EXIT_FAILURE;
                   }
-                }
-                ret_val = fprintf(file, "%d\n%d\n", seq->rnum, elapsed_time);
-                if (ret_val < 0) {
-                  perror("fprintf of number and time failed");
-                  fclose(file);
-                  return EXIT_FAILURE;
                 }
                 ret_val = fclose(file);
                 if (ret_val != 0) {
@@ -79,8 +77,6 @@ int main(int argc, char** argv)
                 }
                 ccv_array_free(seq);
                 ccv_matrix_free(image);
-                // added block
-
 	} else {
 		FILE* r = fopen(argv[1], "rt");
 		if (argc == 4)
@@ -104,14 +100,10 @@ int main(int argc, char** argv)
 					ccv_comp_t* comp = (ccv_comp_t*)ccv_array_get(seq, i);
 					printf("%s %d %d %d %d %f\n", file, comp->rect.x, comp->rect.y, comp->rect.width, comp->rect.height, comp->classification.confidence);
 				}
-				// ccv_array_free(seq); removed line
-				// ccv_matrix_free(image); removed line
 			}
 			free(file);
 			fclose(r);
 		}
-
-                // added block
                 ccv_bbf_classifier_cascade_free(cascade);
                 ccv_disable_cache();
                 accept_roi_end();
@@ -137,9 +129,6 @@ int main(int argc, char** argv)
                 }
                 ccv_array_free(seq);
                 ccv_matrix_free(image);
-                // added block
 	}
-	// ccv_bbf_classifier_cascade_free(cascade); removed line
-	// ccv_disable_cache(); removed line
 	return 0;
 }
--- apps/ccv/eval.py
+++ apps/ccv/eval.py
@@ -1,13 +1,22 @@
+import math
+
 def load():
   out = []
   with open('my_output.txt') as f:
     for line in f:
-      first_num = line
-      out.append(float(first_num))
+      out.append(line)
   return out
 
 def score(orig, relaxed):
   total = 0.0
+  diagonal = math.sqrt(math.pow(2400, 2) + math.pow(1745, 2))
   for a, b in zip(orig, relaxed):
-    total += min(abs(a - b), 1.0)
+    x_a, y_a = a.split(None, 1)
+    x_b, y_b = b.split(None, 1)
+    x_a = float(x_a)
+    y_a = float(y_a)
+    x_b = float(x_b)
+    y_b = float(y_b)
+    distance = math.sqrt(math.pow((x_b - x_a), 2) + math.pow((y_b - y_a), 2))
+    total += min(distance / diagonal, 1.0)
   return total / len(orig)
