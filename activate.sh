dbd4c7c132352791dc1c3aa1b2d061e703d12555
Chris Fensch <c.fensch@ed.ac.uk>
2014-08-18 23:08:18 +0100

    made activate more foolproof

--- activate.sh
+++ activate.sh
@@ -1,1 +1,14 @@
-export PATH=`pwd`/bin:`pwd`/build/built/bin:$PATH
+#!/bin/bash
+
+called=$_
+
+[[ $called == $0 ]] && echo "You probably want to source this script: '. ${0}"
+
+ACTIVATE_DIR=`dirname ${BASH_SOURCE}`
+ACTIVATE_DIR_NORM="`cd "${ACTIVATE_DIR}";pwd`"
+
+export PATH=${ACTIVATE_DIR_NORM}/bin:${ACTIVATE_DIR_NORM}/build/built/bin:$PATH
+
+unset called
+unset ACTIVATE_DIR
+unset ACTIVATE_DIR_NORM
