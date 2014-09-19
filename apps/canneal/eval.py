9c8970bb7a2ecf54cbe4ee02aa3560b7da727236
Adrian Sampson <adrian@radbox.org>
2014-09-19 13:51:11 -0700

    Remove apps
    They're moving to their own repo.

--- apps/canneal/eval.py
+++
@@ -1,8 +0,0 @@
-from __future__ import division
-
-def load(fn='output.txt'):
-    with open(fn) as f:
-        return float(f.read())
-
-def score(orig, relaxed):
-    return relaxed / orig - 1.0
--- apps/strawman2/eval.py
+++
@@ -1,8 +0,0 @@
-from __future__ import division
-from random import random
-
-def load():
-    return 'file:output.txt'
-
-def score(orig, relaxed):
-    return 0.75 + random() / 4
