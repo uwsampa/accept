5804c6e25df1122c0d6532d59361f915addfb358
Thierry Moreau <moreau@uw.edu>
2015-09-01 15:34:43 -0400

    bitmask increment cannot exceed the remaining available bits

--- bin/tune_precision.py
+++ bin/tune_precision.py
@@ -553,6 +553,7 @@ def tune_lomask(base_config, target_error, passlimit, instlimit, clusterworkers,
                 output_path = tmpoutputsdir+'/'+'out_'+str(tuning_pass)+'_'+str(idx)+EXT
                 logging.debug ("File output path of instruction {}: {}".format(tmp_config[idx]['lomask'], output_path))
                 # Increment the LSB mask value
+                tmp_config[idx]['rate'] = min(tmp_config[idx]['rate'], get_bitwidth_from_type(base_config[idx]['type'])-(base_config[idx]['himask']+base_config[idx]['lomask']))
                 tmp_config[idx]['lomask'] += tmp_config[idx]['rate']
                 logging.info ("Testing lomask of value {} on instruction {}".format(tmp_config[idx]['lomask'], idx))
                 # Test the config
