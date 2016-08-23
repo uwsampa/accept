cad0fd493895f2fb2a0a906fa9f97d6cd4f53aed
Thierry Moreau <moreau@uw.edu>
2016-08-23 13:09:26 -0700

    adding ability to evaluate function from DFG

--- bin/axe.py
+++ bin/axe.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 import argparse
+import inspect
 import logging
+import numpy as np
 import os
 import shlex
 import subprocess32 as subprocess
 
+from functools import partial
 
 # LLVM instruction categories
 controlInsn = ['br','switch','indirectbr','ret']
@@ -20,6 +23,9 @@ cmpInsn = ['icmp','fcmp']
 callInsn = ['call']
 phiInsn = ['phi']
 otherInsn = ['select','va_arg','landingpad','catchpad','cleanuppad']
+
+# Standard math
+stdMathFunc = ["abs","labs","llabs","div","ldiv","lldiv","imaxabs","imaxdiv","fabs","fabsf","fabsl","fmod","fmodf","fmodl","remainder","remainderf","remainderl","remquo","remquof","remquol","fma","fmaf","fmal","fmax","fmaxf","fmaxl","fmin","fminf","fminl","fdim","fdimf","fdiml","exp","expf","expl","exp2","exp2f","exp2l","expm1","expm1f","expm1l","log","logf","logl","log10","log10f","log10l","log2","log2f","log2l","log1p","log1pf","log1pl","pow","powf","powl","sqrt","sqrtf","sqrtl","cbrt","cbrtf","cbrtl","hypot","hypotf","hypotl","sin","sinf","sinl","cos","cosf","cosl","tan","tanf","tanl","asin","asinf","asinl","acos","acosf","acosl","atan","atanf","atanl","atan2","atan2f","atan2l","sinh,""sinhf,""sinhl,""cosh,""coshf,""coshl,""tanh,""tanhf,""tanhl,""asinh,""asinhf,""asinhl,""acosh,""acoshf,""acoshl,""atanh,""atanhf,""atanhl,""ceil","ceilf","ceill","floor","floorf","floorl","trunc","truncf","truncl","round","lround","llround","nearbyint","nearbyintf","nearbyintl","rint","rintf","rintl","lrint","lrintf","lrintl","llrint","llrintf","llrintl","frexp","frexpf","frexpl","ldexp","ldexpf","ldexpl","modf","modff","modfl","scalbn","scalbnf","scalbnl","scalbln","scalblnf","scalblnl","ilogb","ilogbf","ilogbl","logb","logbf","logbl","nextafter","nextafterf","nextafterl","nexttoward","nexttowardf","nexttowardl","copysign","copysignf","copysignl"]
 
 # Destination files
 LOG_FILE = 'axe.log'
@@ -76,10 +82,54 @@ def labelDotNode(fp, insn):
     fp.write('shape=\"{}\" '.format(shape))
     fp.write('style=\"rounded\"]\n')
 
+#################################################
+# Helper for constant handling
+#################################################
+
+def isFloat(reg):
+    try:
+        float(reg)
+        return True
+    except:
+        return False
+
+def isConstant(reg):
+    if reg=="null":
+        return True
+    elif isFloat(reg):
+        return True
+    else:
+        return False
+
+#################################################
+# Supported functions
+#################################################
+
+funcMap = {
+    'fadd': lambda x, y: x+y,
+    'fsub': lambda x, y: x-y,
+    'fmul': lambda x, y: x*y,
+    'fdiv': lambda x, y: x/y,
+    'store': lambda x: x,
+    'sqrtf': lambda x: np.sqrt(x)
+}
 
 #################################################
 # Class definitions
 #################################################
+
+class Cst:
+    """ Constant class """
+    val = None
+
+    def __str__(self):
+        return str(self.val)
+
+    def __init__(self, cst):
+        self.val = cst
+
+    def evaluate(self, *x):
+        return float(self.val)
 
 class Insn:
     """ Instruction class """
@@ -97,13 +147,16 @@ class Insn:
     callee = None
     addr = None
 
+    # Load Index - used for evaluation of load instructions
+    load_idx = None
+
     def __str__(self):
         label='('+self.bb_id+') '
         if self.dst:
             label += self.dst + ' = '
         label += self.op + ' '
         if self.op == 'call':
-            label += self.fn + '('
+            label += self.callee + '('
         if self.op == 'phi':
             src = ['[' + x[0] + ', ' + x[1] + ']' for x in zip(self.src, self.src_bb_id)]
             label += ', '.join(src)
@@ -116,6 +169,8 @@ class Insn:
         return label
 
     def __init__(self, tokens):
+        # Initialize function to identity
+        self.func = lambda x: x
         # Process token
         self.fn = tokens[0]
         self.op = tokens[1]
@@ -195,11 +250,32 @@ class Insn:
         if not p in self.successors:
             self.successors.append(p)
 
+    def evaluate(self, *x):
+        # Determine op function
+        op = self.op
+        # Pre-process arguments
+        arg = [ p.evaluate(*x) for p in self.predecessors]
+        # Load - retrieve input i
+        if op=='load':
+            return x[self.load_idx]
+        elif op in binaryInsn:
+            return funcMap[op](arg[0], arg[1])
+        else:
+            return funcMap[op](arg[0])
+
 class DDDG:
     """ DDDG class """
+    # Instruction map
     instructions = {}
+    constants = {}
+    # Entry/exit points of target instructions
+    entries = []
+    exits = []
 
     def __init__(self, target=None, fn=ACCEPT_STATIC_FILE):
+
+        # Initialization (load index)
+        load_idx = 0
 
         # If the file does not exist, generate it
         if not os.path.isfile(fn):
@@ -219,13 +295,35 @@ class DDDG:
                 instruction = Insn(tokens)
                 if instruction.dst:
                     if target and instruction.fn==target or not target:
-                        self.instructions[instruction.dst] = instruction
+                        # Add the instruction to the instruction map
+                        i_id = instruction.dst
+                        self.instructions[i_id] = instruction
+                        # Add load instructions to entry point
+                        if instruction.op == 'load':
+                            self.entries.append(i_id)
+                            instruction.load_idx = load_idx
+                            load_idx += 1
+                        elif instruction.op == 'store':
+                            self.exits.append(i_id)
+
+        # Constant initialization
+        for dst, insn in self.instructions.iteritems():
+            if insn.src:
+                for s in insn.src:
+                    if isConstant(s) and not s in self.constants:
+                        self.constants[s] = Cst(s)
 
         # Predecessor initialization
         for dst, insn in self.instructions.iteritems():
             if insn.src:
                 for p in insn.src:
-                    insn.predecessors.append(p)
+                    if p in self.instructions:
+                        insn.predecessors.append(self.instructions[p])
+                    elif p in self.constants:
+                        insn.predecessors.append(self.constants[p])
+                    else:
+                        print "Something went wrong!"
+                        exit()
 
         # Successor initialization
         successors = {}
@@ -238,9 +336,10 @@ class DDDG:
         for dst, insn in self.instructions.iteritems():
             if dst in successors:
                 for s in successors[dst]:
-                    insn.successors.append(s)
+                    insn.successors.append(self.instructions[s])
 
     def generateDfg(self, fn=DFG_FILE_ROOT):
+        """ Generates the DFG of the target kernel """
 
         # Dump the DOT description of DFG
         with open(fn+'.dot', 'w') as fp:
@@ -264,6 +363,18 @@ class DDDG:
             logging.error('Something went compiling {}.dot with graphviz'.format(fn))
             exit()
 
+    def evaluate(self, x):
+        """ Evaluate the current DDDG on the input vector """
+
+        # Derive number of inputs:
+        assert len(x)==len(self.entries), "Input vector length mismatch!"
+
+        # Produce output vector
+        y = []
+        for exit in self.exits:
+            y.append(self.instructions[exit].evaluate(*x))
+
+        return y
 
 
 #################################################
@@ -304,9 +415,10 @@ def cli():
     else:
         rootLogger.setLevel(logging.INFO)
 
-    # Process DDDG
+    # # Process DDDG
     dddg = DDDG(args.target)
     dddg.generateDfg()
+    print dddg.evaluate([1, 4])
 
 if __name__ == '__main__':
     cli()
