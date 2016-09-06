2cbd0da74056cff355591db83ac91ae6d63ec3e9
Thierry Moreau <moreau@uw.edu>
2016-09-06 15:47:01 -0700

    fixed point emulation support

--- bin/axe.py
+++ bin/axe.py
@@ -40,6 +40,15 @@ MAKE_ORIG = 'make run_orig'
 # Alignment used for packing/unpacking
 ALIGN='>'
 
+# IEEE constants
+FLOAT_MANTISSA_W = 23
+FLOAT_EXPONENT_W = 8
+FLOAT_EXP_BIAS = ( (1 << (FLOAT_EXPONENT_W-1)) - 1)
+
+DOUBLE_MANTISSA_W = 52
+DOUBLE_EXPONENT_W = 11
+DOUBLE_EXP_BIAS = ( (1 << (DOUBLE_EXPONENT_W-1)) - 1)
+
 #################################################
 # General OS function helpers
 #################################################
@@ -87,6 +96,37 @@ def labelDotNode(fp, insn):
     fp.write('shape=\"{}\" '.format(shape))
     fp.write('style=\"rounded\"]\n')
 
+
+#################################################
+# Config parsing helper
+#################################################
+
+def parse_relax_config(f):
+    """Parse a relaxation configuration from a file-like object.
+    Generates (ident, param) tuples.
+    """
+    for line in f:
+        line = line.strip()
+        if line:
+            param, ident = line.split(None, 1)
+            yield ident, int(param)
+
+def get_param_from_masks(himask, lomask, maxexp=0):
+    """Returns parameter from width settings
+    """
+    return (maxexp<<16) + (himask<<8) + lomask
+
+def get_masks_from_param(param):
+    """Returns mask width settings from parameter
+    """
+    if param==0:
+        return 0,0,0
+    maxexp = (param>>16) & 0x0000FFFF;
+    param &= 0x0000FFFF
+    himask = (param >> 8) & 0xFF;
+    lomask = (param & 0xFF);
+    return himask, lomask, maxexp
+
 #################################################
 # Helpers
 #################################################
@@ -99,7 +139,7 @@ def isFloat(reg):
         return False
 
 def isConstant(reg):
-    if reg=="null":
+    if reg=='null':
         return True
     elif isFloat(reg):
         return True
@@ -117,7 +157,7 @@ def getValFromBits(raw, ty):
     else:
         assert False, 'Unsupported type {}'.format(ty)
 
-def approxVal(val, ty, prec):
+def approxVal(val, ty, lomask, maxexp):
     """ Approximates the value """
 
     # Convert to hex
@@ -127,12 +167,33 @@ def approxVal(val, ty, prec):
     elif ty=='Double':
         raw =  struct.unpack(ALIGN+'Q', struct.pack(ALIGN+'d', val))[0]
 
+    # Determine the value to shift this by
+    shiftVal = lomask
+    if ty=='Double' or ty=='Float':
+        exp = np.frexp(val)[1] - 1
+        shiftVal += maxexp - exp
+        # HACK: Adjustments
+        if shiftVal < 0: shiftVal = 0
+        if ty=='Double' and shiftVal>DOUBLE_MANTISSA_W:
+            # shiftVal = DOUBLE_MANTISSA_W
+            return 0
+        elif ty=='Float' and shiftVal>FLOAT_MANTISSA_W:
+            # shiftVal = FLOAT_MANTISSA_W
+            return 0
+        logging.debug('lomask: {}, exp: {}, maxexp {}, shiftval: {}'.format(lomask, exp, maxexp, shiftVal))
+
     # Approximate
-    raw >>= prec
-    raw <<= prec
+    raw >>= shiftVal
+    raw <<= shiftVal
+
+    # Corner case
+    if raw==0:
+        return raw
 
     # Convert to float/double
-    raw = str(hex(raw))[2:]
+    raw = str(hex(raw))
+    raw = raw.lstrip('0x')
+    raw = raw.rstrip('L')
     if ty=='Float':
         val = struct.unpack(ALIGN+'f', raw.decode('hex'))[0]
         return np.float32(val)
@@ -223,8 +284,9 @@ class Insn:
         self.i_id = tokens[2]
         self.bb_id = tokens[2].split('i')[0]
         self.qual = tokens[3]
-        # Precision setting
-        self.prec = 0
+        # Precision settings
+        self.lomask = 0
+        self.maxexp = 0
         # If return instruction
         if self.op == 'ret':
             self.src_ty = [tokens[4]]
@@ -285,7 +347,7 @@ class Insn:
         self.successors = []
 
     def isApprox(self):
-        if self.qual=="approx":
+        if self.qual=='approx':
             return True
         else:
             return False
@@ -314,7 +376,7 @@ class Insn:
             retVal = funcMap[op](arg[0])
         # Approximate output value
         ty = self.src_ty[0] if op=='store' else self.dst_ty
-        retVal = approxVal(retVal, ty, self.prec)
+        retVal = approxVal(retVal, ty, self.lomask, self.maxexp)
         return retVal
 
 class DDDG:
@@ -383,7 +445,7 @@ class DDDG:
                     elif p in self.constants:
                         insn.predecessors.append(self.constants[p])
                     else:
-                        print "Something went wrong!"
+                        logging.error('Something went wrong: reg ID does not correspond to instruction or constant!')
                         exit()
 
         # Successor initialization
@@ -436,7 +498,7 @@ class DDDG:
         """ Evaluate the current DDDG on the input vector """
 
         # Derive number of inputs:
-        assert len(x)==len(self.entries), "Input vector length mismatch!"
+        assert len(x)==len(self.entries), 'Input vector length mismatch!'
 
         # Produce output vector
         y = []
@@ -445,7 +507,7 @@ class DDDG:
 
         return y
 
-    def loadMemTrace(self, fn=ACCEPT_LD_FILE, lim=1):
+    def loadMemTrace(self, lim, fn=ACCEPT_LD_FILE):
         """ Reads in memory trace file """
 
         # If the file does not exist, generate it
@@ -467,7 +529,9 @@ class DDDG:
                 # Determine the load/store pair index
                 pairIdx = idx / (numEntries + numExits)
                 # Break early if we reached the quota
-                if pairIdx >= lim:
+                if lim and pairIdx >= lim:
+                    break
+                elif line=='':
                     break
                 # Initialize new load/store value pairs
                 if len(self.memTrace['load'])==pairIdx:
@@ -482,14 +546,14 @@ class DDDG:
                 # Get the memory instruction
                 memInsn = self.instructions[iid]
                 # Handle value according to load/store op
-                if memInsn.op=="load":
+                if memInsn.op=='load':
                     # Get the data type
                     ty = memInsn.dst_ty
                     # Determine load index
                     load_idx = memInsn.load_idx
                     # Push load value in queue
                     self.memTrace['load'][pairIdx][load_idx] = getValFromBits(val, ty)
-                    logging.debug("#{} ty={}, loadIdx={}, iid={}, addr={}, val={}".format(pairIdx, ty, load_idx, iid, addr, val))
+                    logging.debug('#{} ty={}, loadIdx={}, iid={}, addr={}, val={}'.format(pairIdx, ty, load_idx, iid, addr, val))
                 else:
                     # Get the data type
                     ty = memInsn.src_ty[0]
@@ -497,10 +561,40 @@ class DDDG:
                     store_idx = memInsn.store_idx
                     # Push store value in queue
                     self.memTrace['store'][pairIdx][store_idx] = getValFromBits(val, ty)
-                    logging.debug("#{} ty={}, storeIdx={}, iid={}, addr={}, val={}".format(pairIdx, ty, store_idx, iid, addr, val))
+                    logging.debug('#{} ty={}, storeIdx={}, iid={}, addr={}, val={}'.format(pairIdx, ty, store_idx, iid, addr, val))
+
+    def loadConf(self, fn):
+        """ Reads in config file """
+
+        # If there is no file specified we're done
+        if not fn:
+            return
+
+        # Check if the file path exists
+        if not os.path.isfile(fn):
+            logging.error("Config file {} not found!".format(fn))
+            exit()
+
+        # Configuration object
+        config = []
+        with open(fn) as f:
+            for ident, param in parse_relax_config(f):
+                # If this is in a function indicated in the parameters file,
+                # adjust the parameter accordingly.
+                if ident.startswith('instruction'):
+                    _, i_ident = ident.split()
+                    func, bb, line, opcode, typ = i_ident.split(':')
+                    himask, lomask, maxexp = get_masks_from_param(param)
+
+                    # Set precision to lomask
+                    i_id = 'bb{}i{}'.format(bb, line)
+                    self.instructions[i_id].lomask = lomask
+                    self.instructions[i_id].maxexp = maxexp - (DOUBLE_EXP_BIAS if typ=="Double" else FLOAT_EXP_BIAS)
 
     def test(self):
         """ Evaluates kernel error on memory trace """
+
+        logging.info('Evaluating kernel on {} inputs...'.format(len(self.memTrace['load'])))
 
         # Kernel outputs
         outputs = [self.evaluate(x) for x in self.memTrace['load']]
@@ -511,7 +605,8 @@ class DDDG:
         num = ((goldenData) ** 2).sum(axis=None)
         den = ((goldenData - relaxedData) ** 2).sum(axis=None)
         snr = 10 * np.log10( num/den )
-        return snr
+        logging.info('SNR = {}'.format(snr))
+
 
 #################################################
 # Argument validation
@@ -523,7 +618,7 @@ def cli():
     )
     parser.add_argument(
         '-conf', dest='accept_config_fn', action='store', type=str, required=False,
-        default=None, help='accept_config_file'
+        default=None, help='accept config file name'
     )
     parser.add_argument(
         '-d', dest='debug', action='store_true', required=False,
@@ -536,6 +631,10 @@ def cli():
     parser.add_argument(
         '-log', dest='logpath', action='store', type=str, required=False,
         default=LOG_FILE, help='path to log file'
+    )
+    parser.add_argument(
+        '-lim', dest='lim', action='store', type=int, required=False,
+        default=0, help='data set size limit'
     )
     args = parser.parse_args()
 
@@ -556,11 +655,13 @@ def cli():
     dddg.generateDfg()
 
     # Load memory trace
-    dddg.loadMemTrace()
+    dddg.loadMemTrace(args.lim)
+
+    # Load configuration
+    dddg.loadConf(args.accept_config_fn)
 
     # Produce SNR
-    snr = dddg.test()
-    logging.info("SNR = {}".format(snr))
+    dddg.test()
 
 
 if __name__ == '__main__':
